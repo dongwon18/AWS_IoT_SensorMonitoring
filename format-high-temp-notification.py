3ad387ad74cdea46561c9b65e3b6d8c06a4f7cec
DongwonKim <74483608+dongwon18@users.noreply.github.com>
2021-08-26 15:08:36 +0900

    Create format-high-temp-notification.py

---
+++ format-high-temp-notification.py
@@ -0,0 +1,38 @@
+# python 3.7
+
+import boto3
+#
+#   expects event parameter to contain:
+#   {
+#       "device_id": "32",
+#       "reported_temperature": 38,
+#       "max_temperature": 30,
+#       "notify_topic_arn": "arn:aws:sns:us-east-1:57EXAMPLE833:high_temp_notice"
+#   }
+# 
+#   sends a plain text to AWS IoT Core to be used in an email 
+#
+#      "Device {0} reports a temperature of {1}, which exceeds the limit of {2}."
+#   
+#   where:
+#       {0} is the device_id value
+#       {1} is the reported_temperature value
+#       {2} is the max_temperature value
+#
+def lambda_handler(event, context):
+
+    # Create an SNS client to send notification
+    sns = boto3.client('sns')
+
+    # Format text message from data
+    message_text = "Device {0} reports a temperature of {1}, which exceeds the limit of {2}.".format(
+            str(event['device_id']),
+            str(event['reported_temperature']),
+            str(event['max_temperature'])
+        )
+
+    # Publish the formatted message
+    response = sns.publish(
+            TopicArn = event['notify_topic_arn'],
+            Message = message_text
+        )
